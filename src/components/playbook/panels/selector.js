cfa355b93835732bd5f28db70f18002d790a64bf
runtu <huorunquan@126.com>
2019-08-21 18:03:04 +0800

    暂存

--- src/components/playbook/panels/selector.js
+++ src/components/playbook/panels/selector.js
@@ -4,21 +4,22 @@ import $ from 'jquery'
 import _ from 'underscore'
 
 import Base from './base'
+import { defaultProps } from '../nodes/base'
 
 const template = `<h3>What type of block would you like to add?</h3>
 <div class="box">
   <h5>Execute Actions</h5>
   <div class="row">
     <span class="block" data-block="action">
-      <img src="/inc/coa/img/selector_action_<%= theme %>.svg">
+      <img src="${defaultProps.imgPrefix}/selector_action_<%= theme %>.svg">
       <p>Action</p>
     </span>
-    <span class="block" data-block="playbook">
-      <img src="/inc/coa/img/selector_playbook_<%= theme %>.svg">
+    <span class="block hidden" data-block="playbook">
+      <img src="${defaultProps.imgPrefix}/selector_playbook_<%= theme %>.svg">
       <p>Playbook</p>
     </span>
-    <span class="block" data-block="api">
-      <img src="/inc/coa/img/selector_api_<%= theme %>.svg">
+    <span class="block hidden" data-block="api">
+      <img src="${defaultProps.imgPrefix}/selector_api_<%= theme %>.svg">
       <p>API</p>
     </span>
     <div class="clear"></div>
@@ -28,29 +29,29 @@ const template = `<h3>What type of block would you like to add?</h3>
   <h5>Process Filters</h5>
   <div class="row">
     <span class="block" data-block="filter">
-      <img src="/inc/coa/img/selector_filter_<%= theme %>.svg">
+      <img src="${defaultProps.imgPrefix}/selector_filter_<%= theme %>.svg">
       <p>Filter</p>
     </span>
-    <span class="block" data-block="decision">
-      <img src="/inc/coa/img/selector_decision_<%= theme %>.svg">
+    <span class="block hidden" data-block="decision">
+      <img src="${defaultProps.imgPrefix}/selector_decision_<%= theme %>.svg">
       <p>Decisions</p>
     </span>
-    <span class="block" data-block="format">
-      <img src="/inc/coa/img/selector_format_<%= theme %>.svg">
+    <span class="block hidden" data-block="format">
+      <img src="${defaultProps.imgPrefix}/selector_format_<%= theme %>.svg">
       <p>Format</p>
     </span>
     <div class="clear"></div>
   </div>
 </div>
-<div class="box">
+<div class="box hidden">
   <h5>Human Input</h5>
   <div class="row">
     <span class="block" data-block="prompt">
-      <img src="/inc/coa/img/selector_prompt_<%= theme %>.svg">
+      <img src="${defaultProps.imgPrefix}/selector_prompt_<%= theme %>.svg">
       <p>Prompt</p>
     </span>
     <span class="block" data-block="task">
-      <img src="/inc/coa/img/selector_task_<%= theme %>.svg">
+      <img src="${defaultProps.imgPrefix}/selector_task_<%= theme %>.svg">
       <p>Manual Task</p>
     </span>
     <div class="clear"></div>
@@ -58,15 +59,15 @@ const template = `<h3>What type of block would you like to add?</h3>
 </div>
 <% if (canEditCode) { %>
   <div class="box">
-  <h5>Custom Code</h5>
-  <div class="row">
-    <span class="block" data-block="functionBlock">
-      <img src="/inc/coa/img/selector_custom_function_<%= theme %>.svg">
-      <p>Custom Function</p>
-    </span>
-    <div class="clear"></div>
+    <h5>Custom Code</h5>
+    <div class="row">
+      <span class="block" data-block="functionBlock">
+        <img src="${defaultProps.imgPrefix}/selector_custom_function_<%= theme %>.svg">
+        <p>Custom Function</p>
+      </span>
+      <div class="clear"></div>
+    </div>
   </div>
-</div>
 <% } %>
 `
 
